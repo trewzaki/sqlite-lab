a97266606facb551a3fe7b7960d15021b783a1b4
Tananut Panyagosa <tongtananut@gmail.com>
2020-02-04 11:43:42 +0700

    Update User model and add .gitignore

--- models/user.go
+++ models/user.go
@@ -1,8 +1,15 @@
 package models
 
+import "time"
+
 type User struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName   string     `json:"first_name"`
+	LastName    string     `json:"last_name"`
+	Email       string     `json:"email"`
+	PhoneNumber *string    `json:"phone_number"`
+	CreatedAt   time.Time  `gorm:"DEFAULT:now()" json:"created_at"`
+	UpdatedAt   *time.Time `gorm:"DEFAULT:now()" json:"updated_at"`
+	DeletedAt   *time.Time `gorm:"DEFAULT:NULL" json:"deleted_at"`
 }
 
 func (user *User) Create() error {
